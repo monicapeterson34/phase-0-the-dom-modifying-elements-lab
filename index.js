55560aa3f8ee4a3b390af513efd8af354ebe817e
Monica Peterson <monicapeterson2749@gmail.com>
2023-01-27 15:04:16 -0600

    clear fjla;dlfkj

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const newHeader = document.createElement("h1");
+const main = document.getElementById("main");
+main.remove();
+
+newHeader.id = "victory";
+newHeader.textContent = "Monica is the champion";
